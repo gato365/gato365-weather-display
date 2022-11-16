6747fa513e43219d363b54abbcfc0669dd93440c
gato365 <immanuelwilliams@gato365.com>
2022-11-16 09:59:25 -0800

    Created the capacity to change the html using DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,13 @@ var tmpcityName = document.querySelector('#cityName');
 var tmpsubmitBtn = document.querySelector('#submitBtn');
 var cityName;
 var requestUrl;
+var fiveDayForecast = [];
 var day0 = document.querySelector('#day-0');
+var day1 = document.querySelector('#day-1');
+var day2 = document.querySelector('#day-2');
+var day3 = document.querySelector('#day-3');
+var day4 = document.querySelector('#day-4');
+var day5 = document.querySelector('#day-5');
 
 
 
@@ -45,32 +51,79 @@ tmpsubmitBtn.addEventListener("click", function () {
     cityName = tmpcityName.value;
     console.log(cityName);
     day0.innerHTML = cityName;
-   
-   // Copy Paste 6 times
-    const info = day0.nextElementSibling;
-    
-    info.children[0].innerHTML = 'Temp--';
-    info.children[1].innerHTML = 'Wind--';
-    info.children[2].innerHTML = 'Humidity--';
+
+
+
+
 
     requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=' + apiKey;
     getapi();
-    
+
+    // Copy Paste 6 times
+
+
+    var cityInfoString = localStorage.getItem('description' + cityName);
+    var cityInfoJSON = JSON.parse(cityInfoString);
+    console.log(cityInfoJSON);
+
+    // I will refractor this
+    // Day 0
+    const info0 = day0.nextElementSibling;
+    info0.children[0].innerHTML = 'Temp--';
+    info0.children[1].innerHTML = 'Wind--';
+    info0.children[2].innerHTML = 'Humidity--';
+
+
+    // Day 1
+    const info1 = day1.children[0];
+    info1.children[0].innerHTML = 'Temp--';
+    info1.children[1].innerHTML = 'Wind--';
+    info1.children[2].innerHTML = 'Humidity--';
+
+
+    // Day 2
+    const info2 = day2.children[0];
+    info2.children[0].innerHTML = 'Temp--';
+    info2.children[1].innerHTML = 'Wind--';
+    info2.children[2].innerHTML = 'Humidity--';
+
+
+    // Day 3
+    const info3 = day3.children[0];
+    info3.children[0].innerHTML = 'Temp--';
+    info3.children[1].innerHTML = 'Wind--';
+    info3.children[2].innerHTML = 'Humidity--';
+
+    // Day 4
+    const info4 = day4.children[0];
+    info4.children[0].innerHTML = 'Temp--';
+    info4.children[1].innerHTML = 'Wind--';
+    info4.children[2].innerHTML = 'Humidity--';
+
+
+    // Day 5
+    const info5 = day5.children[0];
+    info5.children[0].innerHTML = 'Temp--';
+    info5.children[1].innerHTML = 'Wind--';
+    info5.children[2].innerHTML = 'Humidity--';
+
 });
 
 
-function updateDayInfo(info,ts,ws,hu){
+// function updateDayInfo(info,ts,ws,hu){
 
-    info.children[0].innerHTML = 'Temp:' + ts;
-    info.children[1].innerHTML = 'Wind:' + ts;
-    info.children[2].innerHTML = 'Humidity:' + hu;
-    
+//     info.children[0].innerHTML = 'Temp:' + ts;
+//     info.children[1].innerHTML = 'Wind:' + ts;
+//     info.children[2].innerHTML = 'Humidity:' + hu;
 
-}
+
+// }
+
+
 
 
 function getapi() {
-    var fiveDayForecast = [];
+
     fetch(requestUrl)
 
 
@@ -83,7 +136,7 @@ function getapi() {
             return response.json();
         })
         .then(function (data) {
-        
+
             // Gets Lat and Long from data
             const latInfo = data[0].lat;
             const lonInfo = data[0].lon;
@@ -119,6 +172,7 @@ function getapi() {
 
                     }
 
+                    // console.log(fiveDayForecast);
                     // Display Information
                     const weatherInfo = JSON.stringify(fiveDayForecast)
                     storeCityWeather(cityName, weatherInfo);
